8bbcb56dcd2e90b9c9d4390eaa13bec633f1b3b1
emi87ar <elopezfilippini@gmail.com>
2023-10-23 18:32:49 -0300

    s

--- NuevoArchivo.py
+++ NuevoArchivo.py
@@ -1,1 +1,8 @@
-$saas
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot  as plt
+import numpy as np
+from sklearn.impute import KNNImputer
+
+df = pd.read_csv(r"C:\Users\BUCEFALO\Documents\Pyhthonworks\titanic.csv")
+print(df.head())
